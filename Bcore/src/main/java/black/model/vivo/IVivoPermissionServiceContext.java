49a916a09ff9a22427783fac1924fbcdffddd1ac
AKRO <habiba31245@gmail.com>
2026-09-11 15:02:18 +0300

    Delete Bcore/src/main/java/black/model/vivo/IVivoPermissionServiceContext.java

--- Bcore/src/main/java/black/model/vivo/IVivoPermissionServiceContext.java
+++
@@ -1,8 +0,0 @@
-package black.model.vivo;
-
-import top.niunaijun.blackreflection.annotation.BClassNameNotProcess;
-
-/* JADX INFO: loaded from: classes3.dex */
-@BClassNameNotProcess("vivo.app.security.IVivoPermissionService")
-public interface IVivoPermissionServiceContext {
-}
