528ef5179456728ec62d3b6d7f880f077d37a00c
AKRO <habiba31245@gmail.com>
2026-09-11 15:31:42 +0300

    Add files via upload

--- Bcore/src/main/java/top/niunaijun/blackbox/utils/compat/XposedParserCompat.java
+++ Bcore/src/main/java/top/niunaijun/blackbox/utils/compat/XposedParserCompat.java
@@ -7,15 +7,12 @@ import top.niunaijun.blackbox.entity.pm.InstalledModule;
 import top.niunaijun.blackbox.utils.CloseUtils;
 import top.niunaijun.blackbox.utils.ShellUtils;
 import java.io.BufferedReader;
-import java.io.Closeable;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-/* JADX INFO: loaded from: classes3.dex */
 public class XposedParserCompat {
     public static InstalledModule parseModule(ApplicationInfo applicationInfo) {
         try {
@@ -23,11 +20,13 @@ public class XposedParserCompat {
             InstalledModule installedModule = new InstalledModule();
             installedModule.packageName = applicationInfo.packageName;
             installedModule.enable = false;
-            installedModule.desc = applicationInfo.metaData.getString("xposeddescription");
+            if (applicationInfo.metaData != null) {
+                installedModule.desc = applicationInfo.metaData.getString("xposeddescription");
+            }
             installedModule.name = applicationInfo.loadLabel(packageManager).toString();
             installedModule.main = readMain(applicationInfo.sourceDir);
             return installedModule;
-        } catch (RuntimeException unused) {
+        } catch (Throwable unused) {
             return null;
         }
     }
@@ -36,78 +35,43 @@ public class XposedParserCompat {
         return readMain(str) != null;
     }
 
-    /* JADX WARN: Not initialized variable reg: 3, insn: 0x0041: MOVE (r2 I:??[OBJECT, ARRAY]) = (r3 I:??[OBJECT, ARRAY]), block:B:21:0x0041 */
-    private static String readMain(String str) throws Throwable {
-        ZipFile zipFile;
-        Closeable closeable;
-        Closeable closeable2 = null;
+    private static String readMain(String str) {
+        ZipFile zipFile = null;
         try {
-            try {
-                zipFile = new ZipFile(new File(str));
-                try {
-                    ZipEntry entry = zipFile.getEntry("assets/xposed_init");
-                    if (entry == null) {
-                        throw new RuntimeException();
-                    }
-                    String strTrim = getInputStreamContent(zipFile.getInputStream(entry)).trim();
-                    CloseUtils.close(zipFile);
-                    return strTrim;
-                } catch (IOException e) {
-                    e = e;
-                    e.printStackTrace();
-                    CloseUtils.close(zipFile);
-                    return null;
-                }
-            } catch (Throwable th) {
-                th = th;
-                closeable2 = closeable;
-                CloseUtils.close(closeable2);
-                throw th;
+            zipFile = new ZipFile(new File(str));
+            ZipEntry entry = zipFile.getEntry("assets/xposed_init");
+            if (entry == null) {
+                return null;
             }
-        } catch (IOException e2) {
-            e = e2;
-            zipFile = null;
-        } catch (Throwable th2) {
-            th = th2;
-            CloseUtils.close(closeable2);
-            throw th;
+            return getInputStreamContent(zipFile.getInputStream(entry)).trim();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            CloseUtils.close(zipFile);
         }
     }
 
-    private static String getInputStreamContent(InputStream inputStream) throws Throwable {
+    private static String getInputStreamContent(InputStream inputStream) {
+        if (inputStream == null) {
+            return "";
+        }
         StringBuilder sb = new StringBuilder();
         BufferedReader bufferedReader = null;
         try {
-            try {
-                BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(inputStream));
-                while (true) {
-                    try {
-                        String line = bufferedReader2.readLine();
-                        if (line == null) {
-                            break;
-                        }
-                        if (!line.startsWith("#")) {
-                            sb.append(line).append(ShellUtils.COMMAND_LINE_END);
-                        }
-                    } catch (Exception e) {
-                        e = e;
-                        bufferedReader = bufferedReader2;
-                        e.printStackTrace();
-                        CloseUtils.close(bufferedReader);
-                    } catch (Throwable th) {
-                        th = th;
-                        bufferedReader = bufferedReader2;
-                        CloseUtils.close(bufferedReader);
-                        throw th;
-                    }
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (!line.startsWith("#")) {
+                    sb.append(line).append(ShellUtils.COMMAND_LINE_END);
                 }
-                CloseUtils.close(bufferedReader2);
-            } catch (Exception e2) {
-                e = e2;
             }
-            return sb.toString();
-        } catch (Throwable th2) {
-            th = th2;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            CloseUtils.close(bufferedReader);
+            CloseUtils.close(inputStream);
         }
+        return sb.toString();
     }
 }
