31d769eadf5f8875442b7451cff84a234dd331b7
AKRO <habiba31245@gmail.com>
2026-09-11 15:05:11 +0300

    Delete Bcore/src/main/java/black/android/app/ILocaleManagerContext.java

--- Bcore/src/main/java/black/android/app/ILocaleManagerContext.java
+++
@@ -1,8 +0,0 @@
-package black.android.app;
-
-import top.niunaijun.blackreflection.annotation.BClassNameNotProcess;
-
-/* JADX INFO: loaded from: classes3.dex */
-@BClassNameNotProcess("android.app.ILocaleManager")
-public interface ILocaleManagerContext {
-}
