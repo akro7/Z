a4a4101ede771f627f8deed3d53de2c0141b20fc
AKRO <habiba31245@gmail.com>
2026-09-11 14:47:33 +0300

    Add files via upload

---
+++ Bcore/src/main/java/top/niunaijun/blackbox/app/GoogleSignInHelper.java
@@ -0,0 +1,215 @@
+package top.niunaijun.blackbox.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.ResultReceiver;
+import android.util.Base64;
+import androidx.autofill.HintConstants;
+import androidx.core.app.NotificationCompat;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import org.json.JSONObject;
+
+/* JADX INFO: loaded from: classes3.dex */
+public class GoogleSignInHelper {
+    public static final String ACTION_GSI_CANCEL = "com.zoro.engine.GSI_CANCEL";
+    public static final String ACTION_GSI_TOKEN = "com.zoro.engine.GSI_TOKEN";
+    private static final String BUNDLE_KEY_DISPLAY_NAME = "com.google.android.libraries.identity.googleid.BUNDLE_KEY_DISPLAY_NAME";
+    private static final String BUNDLE_KEY_FAMILY_NAME = "com.google.android.libraries.identity.googleid.BUNDLE_KEY_FAMILY_NAME";
+    private static final String BUNDLE_KEY_GIVEN_NAME = "com.google.android.libraries.identity.googleid.BUNDLE_KEY_GIVEN_NAME";
+    private static final String BUNDLE_KEY_ID = "com.google.android.libraries.identity.googleid.BUNDLE_KEY_ID";
+    private static final String BUNDLE_KEY_ID_TOKEN = "com.google.android.libraries.identity.googleid.BUNDLE_KEY_ID_TOKEN";
+    private static final String BUNDLE_KEY_PROFILE_PICTURE_URI = "com.google.android.libraries.identity.googleid.BUNDLE_KEY_PROFILE_PICTURE_URI";
+    private static final String BUNDLE_KEY_SUBTYPE = "com.google.android.libraries.identity.googleid.BUNDLE_KEY_GOOGLE_ID_TOKEN_SUBTYPE";
+    private static final String TOKEN_FILE = "gsi_cached_token";
+    private static final String TYPE_GOOGLE_ID_TOKEN = "com.google.android.libraries.identity.googleid.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL";
+    private static final String TYPE_SIWG = "com.google.android.libraries.identity.googleid.TYPE_GOOGLE_ID_TOKEN_SIWG_CREDENTIAL";
+
+    public static void cacheToken(Context context, String str) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(context.getFilesDir(), TOKEN_FILE));
+            fileOutputStream.write(str.getBytes("UTF-8"));
+            fileOutputStream.close();
+        } catch (Exception unused) {
+        }
+    }
+
+    public static String getCachedIdToken(Context context) {
+        try {
+            File file = new File(context.getFilesDir(), TOKEN_FILE);
+            if (!file.exists()) {
+                return null;
+            }
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] bArr = new byte[(int) file.length()];
+            fileInputStream.read(bArr);
+            fileInputStream.close();
+            String str = new String(bArr, "UTF-8");
+            if (!isTokenExpired(str)) {
+                return str;
+            }
+            file.delete();
+        } catch (Exception unused) {
+        }
+        return null;
+    }
+
+    public static boolean isTokenExpired(String str) {
+        try {
+            String[] strArrSplit = str.split("\\.");
+            if (strArrSplit.length < 2) {
+                return true;
+            }
+            return new JSONObject(new String(Base64.decode(strArrSplit[1], 9))).optLong("exp", 0L) * 1000 < System.currentTimeMillis();
+        } catch (Exception unused) {
+            return true;
+        }
+    }
+
+    public static void deliverTokenViaCallback(Object obj, String str) throws Exception {
+        String[] strArrSplit = str.split("\\.");
+        if (strArrSplit.length < 2) {
+            throw new IllegalArgumentException("Invalid JWT");
+        }
+        JSONObject jSONObject = new JSONObject(new String(Base64.decode(strArrSplit[1], 9)));
+        String strOptString = jSONObject.optString(NotificationCompat.CATEGORY_EMAIL, jSONObject.optString("sub", ""));
+        String strOptString2 = jSONObject.optString(HintConstants.AUTOFILL_HINT_NAME, "");
+        String strOptString3 = jSONObject.optString("given_name", "");
+        String strOptString4 = jSONObject.optString("family_name", "");
+        String strOptString5 = jSONObject.optString("picture", "");
+        Bundle bundle = new Bundle();
+        bundle.putString(BUNDLE_KEY_ID, strOptString);
+        bundle.putString(BUNDLE_KEY_ID_TOKEN, str);
+        bundle.putString(BUNDLE_KEY_DISPLAY_NAME, strOptString2);
+        bundle.putString(BUNDLE_KEY_GIVEN_NAME, strOptString3);
+        bundle.putString(BUNDLE_KEY_FAMILY_NAME, strOptString4);
+        bundle.putString(BUNDLE_KEY_SUBTYPE, TYPE_SIWG);
+        if (!strOptString5.isEmpty()) {
+            bundle.putParcelable(BUNDLE_KEY_PROFILE_PICTURE_URI, Uri.parse(strOptString5));
+        }
+        Class<?> cls = Class.forName("android.credentials.Credential");
+        int i = 0;
+        Object objNewInstance = cls.getConstructor(String.class, Bundle.class).newInstance(TYPE_GOOGLE_ID_TOKEN, bundle);
+        Class<?> cls2 = Class.forName("android.credentials.GetCredentialResponse");
+        Object objNewInstance2 = cls2.getConstructor(cls).newInstance(objNewInstance);
+        Method methodFindMethod = findMethod(obj.getClass(), "onResponse", cls2);
+        if (methodFindMethod == null) {
+            Method[] methods = obj.getClass().getMethods();
+            int length = methods.length;
+            while (true) {
+                if (i >= length) {
+                    break;
+                }
+                Method method = methods[i];
+                if ("onResponse".equals(method.getName())) {
+                    methodFindMethod = method;
+                    break;
+                }
+                i++;
+            }
+        }
+        if (methodFindMethod != null) {
+            methodFindMethod.invoke(obj, objNewInstance2);
+        }
+    }
+
+    public static void deliverTokenViaResultReceiver(ResultReceiver resultReceiver, String str, ClassLoader classLoader) throws Exception {
+        Constructor<?> constructor;
+        String[] strArrSplit = str.split("\\.");
+        if (strArrSplit.length < 2) {
+            throw new IllegalArgumentException("Invalid JWT");
+        }
+        JSONObject jSONObject = new JSONObject(new String(Base64.decode(strArrSplit[1], 9)));
+        String strOptString = jSONObject.optString(NotificationCompat.CATEGORY_EMAIL, jSONObject.optString("sub", ""));
+        String strOptString2 = jSONObject.optString(HintConstants.AUTOFILL_HINT_NAME, "");
+        String strOptString3 = jSONObject.optString("given_name", "");
+        String strOptString4 = jSONObject.optString("family_name", "");
+        String strOptString5 = jSONObject.optString("picture", "");
+        Class<?> clsLoadClass = classLoader.loadClass("com.google.android.gms.common.api.Status");
+        Object objNewInstance = clsLoadClass.getDeclaredConstructor(Integer.TYPE).newInstance(0);
+        Class<?> clsLoadClass2 = classLoader.loadClass("com.google.android.gms.auth.api.identity.SignInCredential");
+        Constructor<?>[] declaredConstructors = clsLoadClass2.getDeclaredConstructors();
+        int length = declaredConstructors.length;
+        int i = 0;
+        while (true) {
+            if (i >= length) {
+                constructor = null;
+                break;
+            }
+            Constructor<?> constructor2 = declaredConstructors[i];
+            if (constructor2.getParameterCount() == 9) {
+                constructor = constructor2;
+                break;
+            }
+            i++;
+        }
+        if (constructor == null) {
+            throw new Exception("SignInCredential ctor not found");
+        }
+        constructor.setAccessible(true);
+        Object objNewInstance2 = constructor.newInstance(strOptString, strOptString2, strOptString3, strOptString4, !strOptString5.isEmpty() ? Uri.parse(strOptString5) : null, null, str, null, null);
+        byte[] bArrSafeParcelToBytes = safeParcelToBytes(objNewInstance, clsLoadClass);
+        byte[] bArrSafeParcelToBytes2 = safeParcelToBytes(objNewInstance2, clsLoadClass2);
+        Intent intent = new Intent();
+        intent.putExtra(NotificationCompat.CATEGORY_STATUS, bArrSafeParcelToBytes);
+        intent.putExtra("sign_in_credential", bArrSafeParcelToBytes2);
+        Bundle bundle = new Bundle();
+        bundle.putBoolean("FAILURE_RESPONSE", false);
+        bundle.putInt("ACTIVITY_REQUEST_CODE", 1);
+        bundle.putParcelable("RESULT_DATA", intent);
+        resultReceiver.send(-1, bundle);
+    }
+
+    public static void deliverError(Object obj, String str, String str2) {
+        Method method;
+        try {
+            Method[] methods = obj.getClass().getMethods();
+            int length = methods.length;
+            int i = 0;
+            while (true) {
+                if (i >= length) {
+                    method = null;
+                    break;
+                }
+                method = methods[i];
+                if ("onError".equals(method.getName())) {
+                    break;
+                } else {
+                    i++;
+                }
+            }
+            if (method != null) {
+                method.invoke(obj, str, str2);
+            }
+        } catch (Exception unused) {
+        }
+    }
+
+    public static void launchWebViewSignIn(Context context) {
+        Intent intent = new Intent(context, (Class<?>) GoogleSignInWebViewActivity.class);
+        intent.addFlags(268435456);
+        context.startActivity(intent);
+    }
+
+    private static Method findMethod(Class<?> cls, String str, Class<?>... clsArr) {
+        try {
+            return cls.getMethod(str, clsArr);
+        } catch (NoSuchMethodException unused) {
+            return null;
+        }
+    }
+
+    private static byte[] safeParcelToBytes(Object obj, Class<?> cls) throws Exception {
+        Parcel parcelObtain = Parcel.obtain();
+        cls.getMethod("writeToParcel", Parcel.class, Integer.TYPE).invoke(obj, parcelObtain, 0);
+        byte[] bArrMarshall = parcelObtain.marshall();
+        parcelObtain.recycle();
+        return bArrMarshall;
+    }
+}
---
+++ Bcore/src/main/java/top/niunaijun/blackbox/app/GoogleSignInWebViewActivity.java
@@ -0,0 +1,183 @@
+package top.niunaijun.blackbox.app;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import top.niunaijun.blackbox.BlackBoxCore;
+import java.util.UUID;
+
+/* JADX INFO: loaded from: classes3.dex */
+public class GoogleSignInWebViewActivity extends Activity {
+    private static final String CLIENT_ID = "697261581904-997ch5oh85im8rcq2lt172jbu92gjha6.apps.googleusercontent.com";
+    private static final String FIREBASE_ORIGIN = "https://api-project-697261581904.firebaseapp.com";
+    private static final String REDIRECT_URI = "https://api-project-697261581904.firebaseapp.com/__/auth/handler";
+    private boolean tokenHandled = false;
+    private WebView webView;
+
+    @Override // android.app.Activity
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        String str = "https://accounts.google.com/o/oauth2/v2/auth?client_id=697261581904-997ch5oh85im8rcq2lt172jbu92gjha6.apps.googleusercontent.com&redirect_uri=" + Uri.encode(REDIRECT_URI) + "&response_type=id_token&scope=openid%20email%20profile&nonce=" + UUID.randomUUID().toString().replace("-", "");
+        WebView webView = new WebView(this);
+        this.webView = webView;
+        setContentView(webView);
+        WebSettings settings = this.webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setCacheMode(2);
+        settings.setMixedContentMode(0);
+        String userAgentString = settings.getUserAgentString();
+        if (userAgentString != null) {
+            settings.setUserAgentString(userAgentString.replace("; wv", ""));
+        }
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(this.webView, true);
+        this.webView.addJavascriptInterface(new Object() { // from class: com.samurai.vengine.app.GoogleSignInWebViewActivity.1
+            @JavascriptInterface
+            public void onHash(String str2) {
+                if (str2 == null || !str2.contains("id_token=")) {
+                    return;
+                }
+                GoogleSignInWebViewActivity.this.extractAndDeliver(str2);
+            }
+        }, "TokenBridge");
+        this.webView.setWebChromeClient(new WebChromeClient());
+        this.webView.setWebViewClient(new AnonymousClass2());
+        this.webView.loadUrl(str);
+    }
+
+    /* JADX INFO: renamed from: com.samurai.vengine.app.GoogleSignInWebViewActivity$2, reason: invalid class name */
+    class AnonymousClass2 extends WebViewClient {
+        @Override // android.webkit.WebViewClient
+        public void onReceivedError(WebView webView, WebResourceRequest webResourceRequest, WebResourceError webResourceError) {
+        }
+
+        AnonymousClass2() {
+        }
+
+        @Override // android.webkit.WebViewClient
+        public void onPageFinished(WebView webView, String str) {
+            super.onPageFinished(webView, str);
+            if (str == null || !str.startsWith(GoogleSignInWebViewActivity.REDIRECT_URI)) {
+                return;
+            }
+            webView.evaluateJavascript("(function(){ return window.location.hash; })()", new ValueCallback() { // from class: com.samurai.vengine.app.GoogleSignInWebViewActivity$2$$ExternalSyntheticLambda0
+                @Override // android.webkit.ValueCallback
+                public final void onReceiveValue(Object obj) {
+                    this.f$0.m8760lambda$onPageFinished$0$comsamuraivengineappGoogleSignInWebViewActivity$2((String) obj);
+                }
+            });
+        }
+
+        /* JADX INFO: renamed from: lambda$onPageFinished$0$com-samurai-vengine-app-GoogleSignInWebViewActivity$2, reason: not valid java name */
+        /* synthetic */ void m8760lambda$onPageFinished$0$comsamuraivengineappGoogleSignInWebViewActivity$2(String str) {
+            if (str != null) {
+                String strReplace = str.replace("\"", "");
+                if (strReplace.contains("id_token=")) {
+                    GoogleSignInWebViewActivity.this.extractAndDeliver(strReplace);
+                }
+            }
+        }
+
+        @Override // android.webkit.WebViewClient
+        public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest webResourceRequest) {
+            String fragment;
+            Uri url = webResourceRequest.getUrl();
+            if (url == null || !url.toString().startsWith(GoogleSignInWebViewActivity.REDIRECT_URI) || (fragment = url.getFragment()) == null || !fragment.contains("id_token=")) {
+                return false;
+            }
+            GoogleSignInWebViewActivity.this.extractAndDeliver(fragment);
+            return true;
+        }
+    }
+
+    /* JADX INFO: Access modifiers changed from: private */
+    public void extractAndDeliver(String str) {
+        String strSubstring;
+        if (this.tokenHandled) {
+            return;
+        }
+        this.tokenHandled = true;
+        String[] strArrSplit = str.replace("#", "").split("&");
+        int length = strArrSplit.length;
+        int i = 0;
+        while (true) {
+            if (i >= length) {
+                strSubstring = null;
+                break;
+            }
+            String str2 = strArrSplit[i];
+            if (str2.startsWith("id_token=")) {
+                strSubstring = str2.substring(9);
+                break;
+            }
+            i++;
+        }
+        if (strSubstring == null || strSubstring.isEmpty()) {
+            Intent intent = new Intent(GoogleSignInHelper.ACTION_GSI_CANCEL);
+            intent.setPackage(getPackageName());
+            sendBroadcast(intent);
+        } else {
+            GoogleSignInHelper.cacheToken(BlackBoxCore.getContext(), strSubstring);
+            Intent intent2 = new Intent(GoogleSignInHelper.ACTION_GSI_TOKEN);
+            intent2.putExtra("id_token", strSubstring);
+            intent2.setPackage(getPackageName());
+            sendBroadcast(intent2);
+        }
+        bringGameToFront();
+        finish();
+    }
+
+    @Override // android.app.Activity
+    public void onBackPressed() {
+        WebView webView = this.webView;
+        if (webView != null && webView.canGoBack()) {
+            this.webView.goBack();
+            return;
+        }
+        Intent intent = new Intent(GoogleSignInHelper.ACTION_GSI_CANCEL);
+        intent.setPackage(getPackageName());
+        sendBroadcast(intent);
+        bringGameToFront();
+        super.onBackPressed();
+    }
+
+    @Override // android.app.Activity
+    public void onDestroy() {
+        WebView webView = this.webView;
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    private void bringGameToFront() {
+        try {
+            ActivityManager activityManager = (ActivityManager) getSystemService("activity");
+            if (activityManager == null) {
+                return;
+            }
+            int taskId = getTaskId();
+            for (ActivityManager.AppTask appTask : activityManager.getAppTasks()) {
+                if (appTask.getTaskInfo().id != taskId) {
+                    appTask.moveToFront();
+                    return;
+                }
+            }
+        } catch (Exception unused) {
+        }
+    }
+}
