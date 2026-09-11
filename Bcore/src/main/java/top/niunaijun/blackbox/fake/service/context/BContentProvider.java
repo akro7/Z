123eb799824b47236f86f1b4a134efcd84ac3c0d
AKRO <habiba31245@gmail.com>
2026-09-11 08:46:12 +0300

    Delete Bcore/src/main/java/top/niunaijun/blackbox/fake/service/context/BContentProvider.java

--- Bcore/src/main/java/top/niunaijun/blackbox/fake/service/context/BContentProvider.java
+++
@@ -1,8 +0,0 @@
-package top.niunaijun.blackbox.fake.service.context.providers;
-
-import android.os.IInterface;
-
-/* JADX INFO: loaded from: classes3.dex */
-public interface BContentProvider {
-    IInterface wrapper(IInterface iInterface, String str);
-}
